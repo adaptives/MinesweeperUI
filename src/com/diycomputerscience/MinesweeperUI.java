0c988bafbb2dc9a10154c4ec2d6ed6c0cfec8d5b
Parag Shah <adaptives@gmail.com>
2012-05-24 17:39:58 +0530

    Added a main class for putting the UI code.

---
+++ src/com/diycomputerscience/MinesweeperUI.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.diycomputerscience;
+
+/**
+ * @author pshah
+ *
+ */
+public class MinesweeperUI {
+
+	public static void main(String args[]) {
+		//Create a JFrame and init it here
+	}
+}
