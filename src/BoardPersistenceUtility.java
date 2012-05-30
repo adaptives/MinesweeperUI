27e0589e7c728c76c09b62d18988222f3ab3c164
Parag Shah <adaptives@gmail.com>
2012-05-30 15:07:23 +0530

    created separate class to handle persistence

---
+++ src/BoardPersistenceUtility.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+/**
+ * 
+ */
+
+/**
+ * @author pshah
+ *
+ */
+public class BoardPersistenceUtility {
+	
+	private String SQUARE_FORMAT = "";
+	private String LOAD_REGEX = "";
+	
+	public void save(PrintWriter writer, Board board) {
+		
+	}
+	
+	public Board load(BufferedReader reader) {
+		return null;
+	}
+}
