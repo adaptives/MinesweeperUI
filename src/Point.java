8f4b58c17492fb9c95c9282d42b38033df15102f
Parag Shah <adaptives@gmail.com>
2012-06-01 17:22:02 +0530

    added equals and hashcode to point

--- src/Point.java
+++ src/Point.java
@@ -10,6 +10,32 @@ public class Point {
 	}
 	
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + col;
+		result = prime * result + row;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (col != other.col)
+			return false;
+		if (row != other.row)
+			return false;
+		return true;
+	}
+
+
+	@Override
 	public String toString() {
 		return this.row + "," + this.col;
 	}
